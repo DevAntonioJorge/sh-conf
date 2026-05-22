f951cd3a00637e67bcc6117d6bd2a41844868069
Antonio Jorge <160784200+DevAntonioJorge@users.noreply.github.com>
2026-05-21 21:49:19 -0300

    Fix distro variable initialization in setup.sh

--- setup.sh
+++ setup.sh
@@ -20,8 +20,8 @@ step()  { echo -e "\n\033[1m==> \033[0m\033[1;36m$1\033[0m"; }
 # Distro detection & package manager abstraction
 # ---------------------------------------------------------------------------
 PKG_MGR=""
-DISTRO="generic"
-:
+DISTRO=""
+
 detect_distro() {
   if [[ -f /etc/os-release ]]; then
     source /etc/os-release
